162bb74ae3508836bb7095ab9cf1d1bfd2b779b9
dorami <dennis4079@gmail.com>
2014-02-20 00:08:53 +0900

    다올 최신 소식 위젯 개선

--- modules/admin/tpl/js/admin.js
+++ modules/admin/tpl/js/admin.js
@@ -75,7 +75,7 @@ jQuery(function($){
 
 	// Display the dashboard in two column
 	$(window).resize(function(){
-		if($(document).width() < 1300){
+		if($(document).width() < 1370){
 			$('.dashboard>.section>br').remove();
 			$('.dashboard>.section>.portlet:odd').after('<br style="clear:both" />');
 		} else {
