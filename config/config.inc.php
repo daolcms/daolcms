fe3143481d010627e628dc9bbab68e0b1a03757a
dorami <dennis4079@gmail.com>
2014-05-02 01:04:09 +0900

    Version update: 1.0.0.3

--- config/config.inc.php
+++ config/config.inc.php
@@ -22,7 +22,7 @@
     /**
      * Display the version info of DAOL CMS.
      */
-    define('__DAOL_VERSION__', '1.0.0.2');
+    define('__DAOL_VERSION__', '1.0.0.3');
     /**
      * Display XE's full version.
      */
