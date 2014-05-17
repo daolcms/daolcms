7229bad28f92769dbcdb5c11e6fc0e622287011b
dorami <dennis4079@gmail.com>
2014-05-17 23:42:15 +0900

    첨부 파일 설정 저장 시 Warning 에러 수정

--- modules/file/file.admin.controller.php
+++ modules/file/file.admin.controller.php
@@ -112,6 +112,7 @@
 
             $download_grant = Context::get('download_grant');
 
+            $file_config = new stdClass;
             $file_config->allow_outlink = Context::get('allow_outlink');
             $file_config->allow_outlink_format = Context::get('allow_outlink_format');
             $file_config->allow_outlink_site = Context::get('allow_outlink_site');
