3744e09ffc96ff4f30b5be1dc2e30165cb877c67
dorami <dennis4079@gmail.com>
2014-07-18 17:43:27 +0900

    #20 php4 호환코드 제거

--- classes/file/getRemoteResourcePHP5.php
+++
@@ -1,9 +0,0 @@
-<?php
-try
-{
-	return self::_getRemoteResource($url, $body, $timeout, $method, $content_type, $headers, $cookies, $post_data, $request_config);
-}
-catch(Exception $e)
-{
-	return NULL;
-}
