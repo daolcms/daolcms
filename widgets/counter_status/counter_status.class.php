866082b8a6755e9df008383cacf636916d01c949
Beom Jinhyeok <dennis4079@gmail.com>
2014-09-22 00:59:16 +0900

    widgets 공백을 탭으로

--- widgets/counter_status/counter_status.class.php
+++ widgets/counter_status/counter_status.class.php
@@ -1,24 +1,24 @@
 <?php
-    /**
-     * @class counter_status
-     * @author NHN (developers@xpressengine.com)
-     * @version 0.1
-     * @brief Display counter status by using data in the counter module
-     **/
+	/**
+	 * @class counter_status
+	 * @author NHN (developers@xpressengine.com)
+	 * @version 0.1
+	 * @brief Display counter status by using data in the counter module
+	 **/
 
-    class counter_status extends WidgetHandler {
+	class counter_status extends WidgetHandler {
 
-        /**
-         * @brief Widget execution
-         * Get extra_vars declared in ./widgets/widget/conf/info.xml as arguments
-         * After generating the result, do not print but return it.
-         **/
-        function proc($args) {
-            // Get status of the accumulated, yesterday's, today's counts
-            $oCounterModel = &getModel('counter');
+		/**
+		 * @brief Widget execution
+		 * Get extra_vars declared in ./widgets/widget/conf/info.xml as arguments
+		 * After generating the result, do not print but return it.
+		 **/
+		function proc($args) {
+			// Get status of the accumulated, yesterday's, today's counts
+			$oCounterModel = &getModel('counter');
 
-            $site_module_info = Context::get('site_module_info');
-            $output = $oCounterModel->getStatus(array('00000000', date('Ymd', time()-60*60*24), date('Ymd')), $site_module_info->site_srl);
+			$site_module_info = Context::get('site_module_info');
+			$output = $oCounterModel->getStatus(array('00000000', date('Ymd', time()-60*60*24), date('Ymd')), $site_module_info->site_srl);
 			if(count($output))
 			{
 				foreach($output as $key => $val) 
@@ -28,14 +28,14 @@
 					else Context::set('yesterday_counter', $val);
 				}
 			}
-            // Set a path of the template skin (values of skin, colorset settings)
-            $tpl_path = sprintf('%sskins/%s', $this->widget_path, $args->skin);
-            Context::set('colorset', $args->colorset);
-            // Specify a template file
-            $tpl_file = 'counter_status';
-            // Compile a template
-            $oTemplate = &TemplateHandler::getInstance();
-            return $oTemplate->compile($tpl_path, $tpl_file);
-        }
-    }
+			// Set a path of the template skin (values of skin, colorset settings)
+			$tpl_path = sprintf('%sskins/%s', $this->widget_path, $args->skin);
+			Context::set('colorset', $args->colorset);
+			// Specify a template file
+			$tpl_file = 'counter_status';
+			// Compile a template
+			$oTemplate = &TemplateHandler::getInstance();
+			return $oTemplate->compile($tpl_path, $tpl_file);
+		}
+	}
 ?>
