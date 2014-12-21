831e5127594ef365d9d2bb861a9c059ebe95e22d
Beom Jinhyeok <dennis4079@gmail.com>
2014-12-21 17:01:04 +0900

    Revert "#41 Point, Rss Module blank to tab"
    This reverts commit 79b38d29f052232172a52b42bea8f3a7addba204.

--- modules/poll/poll.admin.view.php
+++ modules/poll/poll.admin.view.php
@@ -1,50 +1,50 @@
 <?php
-	/**
-	 * @class  pollAdminView
-	 * @author NHN (developers@xpressengine.com)
-	 * @brief The admin view class of the poll module
-	 **/
+    /**
+     * @class  pollAdminView
+     * @author NHN (developers@xpressengine.com)
+     * @brief The admin view class of the poll module
+     **/
 
-	class pollAdminView extends poll {
+    class pollAdminView extends poll {
 
-		/**
-		 * @brief Initialization
-		 **/
-		function init() {
-		}
+        /**
+         * @brief Initialization
+         **/
+        function init() {
+        }
 
-		/**
-		 * @brief Administrator's Page
-		 **/
-		function dispPollAdminList() {
-			// Arrange the search options
-			$search_target = trim(Context::get('search_target'));
-			$search_keyword = trim(Context::get('search_keyword'));
+        /**
+         * @brief Administrator's Page
+         **/
+        function dispPollAdminList() {
+            // Arrange the search options
+            $search_target = trim(Context::get('search_target'));
+            $search_keyword = trim(Context::get('search_keyword'));
 
-			if($search_target && $search_keyword) {
-				switch($search_target) {
-					case 'title' :
-							if($search_keyword) $search_keyword = str_replace(' ','%',$search_keyword);
-							$args->s_title= $search_keyword;
-						break;
-					case 'regdate' :
-							$args->s_regdate = $search_keyword;
-						break;
-					case 'ipaddress' :
-							$args->s_ipaddress= $search_keyword;
-						break;
-				}
-			}
-			// Options to get a list of pages
-			$args->page = Context::get('page');
-			$args->list_count = 50; // The number of posts to show on one page
-			$args->page_count = 10; // The number of pages to display in the page navigation
+            if($search_target && $search_keyword) {
+                switch($search_target) {
+                    case 'title' :
+                            if($search_keyword) $search_keyword = str_replace(' ','%',$search_keyword);
+                            $args->s_title= $search_keyword;
+                        break;
+                    case 'regdate' :
+                            $args->s_regdate = $search_keyword;
+                        break;
+                    case 'ipaddress' :
+                            $args->s_ipaddress= $search_keyword;
+                        break;
+                }
+            }
+            // Options to get a list of pages
+            $args->page = Context::get('page');
+            $args->list_count = 50; // The number of posts to show on one page
+            $args->page_count = 10; // The number of pages to display in the page navigation
 
-			$args->sort_index = 'P.list_order'; // Sorting value
+            $args->sort_index = 'P.list_order'; // Sorting value
 
-			// Get the list
-			$oPollAdminModel = &getAdminModel('poll');
-			$output = $oPollAdminModel->getPollListWithMember($args);
+            // Get the list
+            $oPollAdminModel = &getAdminModel('poll');
+            $output = $oPollAdminModel->getPollListWithMember($args);
 
 			// check poll type. document or comment
 			if(is_array($output->data))
@@ -55,7 +55,7 @@
 					array_push($uploadTargetSrlList, $value->upload_target_srl);
 				}
 
-				$oDocumentModel = &getModel('document');
+            	$oDocumentModel = &getModel('document');
 				$targetDocumentOutput = $oDocumentModel->getDocuments($uploadTargetSrlList);
 				if(!is_array($targetDocumentOutput)) $targetDocumentOutput = array();
 
@@ -77,84 +77,84 @@
 				}
 			}
 
-			// Configure the template variables
-			Context::set('total_count', $output->total_count);
-			Context::set('total_page', $output->total_page);
-			Context::set('page', $output->page);
-			Context::set('poll_list', $output->data);
-			Context::set('page_navigation', $output->page_navigation);
-			Context::set('module_list', $module_list);			
+            // Configure the template variables
+            Context::set('total_count', $output->total_count);
+            Context::set('total_page', $output->total_page);
+            Context::set('page', $output->page);
+            Context::set('poll_list', $output->data);
+            Context::set('page_navigation', $output->page_navigation);
+            Context::set('module_list', $module_list);			
 			
 			$security = new Security();				
 			$security->encodeHTML('poll_list..title');
-			// Set a template
-			$this->setTemplatePath($this->module_path.'tpl');
-			$this->setTemplateFile('poll_list');
-		}
+            // Set a template
+            $this->setTemplatePath($this->module_path.'tpl');
+            $this->setTemplateFile('poll_list');
+        }
 
-		/**
-		 * @brief Confgure the poll skin and colorset
-		 **/
-		function dispPollAdminConfig() {
-			$oModuleModel = &getModel('module');
-			// Get the configuration information
-			$config = $oModuleModel->getModuleConfig('poll');
-			Context::set('config', $config);
-			// Get the skin information
-			$skin_list = $oModuleModel->getSkins($this->module_path);
-			Context::set('skin_list', $skin_list);
+        /**
+         * @brief Confgure the poll skin and colorset
+         **/
+        function dispPollAdminConfig() {
+            $oModuleModel = &getModel('module');
+            // Get the configuration information
+            $config = $oModuleModel->getModuleConfig('poll');
+            Context::set('config', $config);
+            // Get the skin information
+            $skin_list = $oModuleModel->getSkins($this->module_path);
+            Context::set('skin_list', $skin_list);
 
-			if(!$skin_list[$config->skin]) $config->skin = "default";
-			// Set the skin colorset once the configurations is completed
-			Context::set('colorset_list', $skin_list[$config->skin]->colorset);
+            if(!$skin_list[$config->skin]) $config->skin = "default";
+            // Set the skin colorset once the configurations is completed
+            Context::set('colorset_list', $skin_list[$config->skin]->colorset);
 			
 			$security = new Security();				
 			$security->encodeHTML('config..');
 			$security->encodeHTML('skin_list..title');
 			$security->encodeHTML('colorset_list..name','colorset_list..title');
 			
-			// Set a template
-			$this->setTemplatePath($this->module_path.'tpl');
-			$this->setTemplateFile('config');
-		}
+            // Set a template
+            $this->setTemplatePath($this->module_path.'tpl');
+            $this->setTemplateFile('config');
+        }
 
-		/**
-		 * @brief Poll Results
-		 **/
-		function dispPollAdminResult() {
-			// Popup layout
-			$this->setLayoutFile("popup_layout");
-			// Draw results
-			$args->poll_srl = Context::get('poll_srl'); 
-			$args->poll_index_srl = Context::get('poll_index_srl'); 
+        /**
+         * @brief Poll Results
+         **/
+        function dispPollAdminResult() {
+            // Popup layout
+            $this->setLayoutFile("popup_layout");
+            // Draw results
+            $args->poll_srl = Context::get('poll_srl'); 
+            $args->poll_index_srl = Context::get('poll_index_srl'); 
 
-			$output = executeQuery('poll.getPoll', $args);
-			if(!$output->data) return $this->stop('msg_poll_not_exists');
-			$poll->stop_date = $output->data->stop_date;
-			$poll->poll_count = $output->data->poll_count;
+            $output = executeQuery('poll.getPoll', $args);
+            if(!$output->data) return $this->stop('msg_poll_not_exists');
+            $poll->stop_date = $output->data->stop_date;
+            $poll->poll_count = $output->data->poll_count;
 
-			$output = executeQuery('poll.getPollTitle', $args);
-			if(!$output->data) return $this->stop('msg_poll_not_exists');
+            $output = executeQuery('poll.getPollTitle', $args);
+            if(!$output->data) return $this->stop('msg_poll_not_exists');
 
-			$poll->poll[$args->poll_index_srl]->title = $output->data->title;
-			$poll->poll[$args->poll_index_srl]->checkcount = $output->data->checkcount;
-			$poll->poll[$args->poll_index_srl]->poll_count = $output->data->poll_count;
+            $poll->poll[$args->poll_index_srl]->title = $output->data->title;
+            $poll->poll[$args->poll_index_srl]->checkcount = $output->data->checkcount;
+            $poll->poll[$args->poll_index_srl]->poll_count = $output->data->poll_count;
 
-			$output = executeQuery('poll.getPollItem', $args);
-			foreach($output->data as $key => $val) {
-				$poll->poll[$val->poll_index_srl]->item[] = $val;
-			}
+            $output = executeQuery('poll.getPollItem', $args);
+            foreach($output->data as $key => $val) {
+                $poll->poll[$val->poll_index_srl]->item[] = $val;
+            }
 
-			$poll->poll_srl = $poll_srl;
+            $poll->poll_srl = $poll_srl;
 
-			Context::set('poll',$poll);
-			// Configure the skin and the colorset for the default configuration
-			$oModuleModel = &getModel('module');
-			$poll_config = $oModuleModel->getModuleConfig('poll');
-			Context::set('poll_config', $poll_config);
+            Context::set('poll',$poll);
+            // Configure the skin and the colorset for the default configuration
+            $oModuleModel = &getModel('module');
+            $poll_config = $oModuleModel->getModuleConfig('poll');
+            Context::set('poll_config', $poll_config);
 
-			$this->setTemplatePath($this->module_path.'tpl');
-			$this->setTemplateFile('result');
-		}
-	}
+            $this->setTemplatePath($this->module_path.'tpl');
+            $this->setTemplateFile('result');
+        }
+    }
 ?>
