7cbbf9e3b2fb78bdf5081fa44b6aa8c8a012f429
Beom Jinhyeok <dennis4079@gmail.com>
2014-12-21 15:49:30 +0900

    #41 Communication, Counter Module blank to tab

--- modules/communication/tpl/js/communication_admin.js
+++ modules/communication/tpl/js/communication_admin.js
@@ -1,14 +1,14 @@
 /* 스킨 컬러셋 구해옴 */
 function doGetSkinColorset(skin) {
-    var params = new Array();
-    params['skin'] = skin;
+	var params = new Array();
+	params['skin'] = skin;
 
-    var response_tags = new Array('error','message','tpl');
-    exec_xml('communication', 'getCommunicationAdminColorset', params, doDisplaySkinColorset, response_tags);
+	var response_tags = new Array('error','message','tpl');
+	exec_xml('communication', 'getCommunicationAdminColorset', params, doDisplaySkinColorset, response_tags);
 }
 
 function doDisplaySkinColorset(ret_obj) {
-    var tpl = ret_obj["tpl"];
-    jQuery('#communication_colorset').html(tpl);
+	var tpl = ret_obj["tpl"];
+	jQuery('#communication_colorset').html(tpl);
 }
 
