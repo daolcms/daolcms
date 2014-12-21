9c1e318818ba8f40187029086b87225fda368a87
Beom Jinhyeok <dennis4079@gmail.com>
2014-12-21 17:10:14 +0900

    #41 Syndication, Tag Module blank to tab

--- modules/syndication/tpl/js/syndication.js
+++ modules/syndication/tpl/js/syndication.js
@@ -1,27 +1,27 @@
 function insertSelectedModules(id, module_srl, mid, browser_title) {
-    var sel_obj = get_by_id('_'+id);
-    for(var i=0;i<sel_obj.options.length;i++) if(sel_obj.options[i].value==module_srl) return;
-    var opt = new Option(browser_title+' ('+mid+')', module_srl, false, false);
-    sel_obj.options[sel_obj.options.length] = opt;
-    if(sel_obj.options.length>8) sel_obj.size = sel_obj.options.length;
+	var sel_obj = get_by_id('_'+id);
+	for(var i=0;i<sel_obj.options.length;i++) if(sel_obj.options[i].value==module_srl) return;
+	var opt = new Option(browser_title+' ('+mid+')', module_srl, false, false);
+	sel_obj.options[sel_obj.options.length] = opt;
+	if(sel_obj.options.length>8) sel_obj.size = sel_obj.options.length;
 
-    doSyncExceptModules(id);
+	doSyncExceptModules(id);
 }
 
 function removeExceptModule(id) {
-    var sel_obj = get_by_id('_'+id);
-    sel_obj.remove(sel_obj.selectedIndex);
-    if(sel_obj.options.length) sel_obj.selectedIndex = sel_obj.options.length-1;
-    doSyncExceptModules(id);
+	var sel_obj = get_by_id('_'+id);
+	sel_obj.remove(sel_obj.selectedIndex);
+	if(sel_obj.options.length) sel_obj.selectedIndex = sel_obj.options.length-1;
+	doSyncExceptModules(id);
 }
 
 function doSyncExceptModules(id) {
-    var selected_module_srls = new Array();
-    var sel_obj = get_by_id('_'+id);
-    for(var i=0;i<sel_obj.options.length;i++) {
-        selected_module_srls.push(sel_obj.options[i].value);
-    }
-    get_by_id(id).value = selected_module_srls.join(',');
+	var selected_module_srls = new Array();
+	var sel_obj = get_by_id('_'+id);
+	for(var i=0;i<sel_obj.options.length;i++) {
+		selected_module_srls.push(sel_obj.options[i].value);
+	}
+	get_by_id(id).value = selected_module_srls.join(',');
 }
 
 
