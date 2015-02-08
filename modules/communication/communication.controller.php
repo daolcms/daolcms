b38178ad7eb4e3de7149fccd9bdefdc5769ff6c8
Beom Jinhyeok <dennis4079@gmail.com>
2015-02-08 17:02:49 +0900

    communication.sendMessage트리거에서 message_srl을 잘못 넘기는 문제 고침

--- modules/communication/communication.controller.php
+++ modules/communication/communication.controller.php
@@ -105,10 +105,14 @@
 		 * @param boolean $sender_log (default true)
 		 * @return Object
 		 **/
-		function sendMessage($sender_srl, $receiver_srl, $title, $content, $sender_log = true) {
+		function sendMessage($sender_srl, $receiver_srl, $title, $content, $sender_log = true){
 			$content = removeHackTag($content); 
-			$title = htmlspecialchars($title);
+			$title = htmlspecialchars($title, ENT_COMPAT | ENT_HTML401, 'UTF-8', false);
+			
+			$message_srl = getNextSequence();
+			$related_srl = getNextSequence();
 			// messages to save in the sendor's message box
+			$sender_args = new stdClass();
 			$sender_args->sender_srl = $sender_srl;
 			$sender_args->receiver_srl = $receiver_srl;
 			$sender_args->message_type = 'S';
@@ -116,13 +120,14 @@
 			$sender_args->content = $content;
 			$sender_args->readed = 'N';
 			$sender_args->regdate = date("YmdHis");
-			$sender_args->related_srl = getNextSequence();
-			$sender_args->message_srl = getNextSequence();
-			$sender_args->list_order = getNextSequence()*-1;
+			$sender_args->message_srl = $message_srl;
+			$sender_args->related_srl = $related_srl;
+			$sender_args->list_order = $sender_args->message_srl * -1;	
 			// messages to save in the receiver's message box
-			$receiver_args->message_srl = $sender_args->related_srl;
+			$receiver_args = new stdClass();
+			$receiver_args->message_srl = $related_srl;
 			$receiver_args->related_srl = 0;
-			$receiver_args->list_order = $sender_args->related_srl*-1;
+			$receiver_args->list_order = $related_srl * -1;
 			$receiver_args->sender_srl = $sender_srl;
 			if(!$receiver_args->sender_srl) $receiver_args->sender_srl = $receiver_srl;
 			$receiver_args->receiver_srl = $receiver_srl;
@@ -130,10 +135,24 @@
 			$receiver_args->title = $title;
 			$receiver_args->content = $content;
 			$receiver_args->readed = 'N';
-			$receiver_args->regdate = date("YmdHis");
-
+			$receiver_args->regdate = date("YmdHis");	
+			// Call a trigger (before)
+			$trigger_obj = new stdClass();
+			$trigger_obj->sender_srl = $sender_srl;
+			$trigger_obj->receiver_srl = $receiver_srl;
+			$trigger_obj->message_srl = $message_srl;
+			$trigger_obj->related_srl = $related_srl;
+			$trigger_obj->title = $title;
+			$trigger_obj->content = $content;
+			$trigger_obj->sender_log = $sender_log;
+			$triggerOutput = ModuleHandler::triggerCall('communication.sendMessage', 'before', $trigger_obj);
+			if(!$triggerOutput->toBool()){
+				return $triggerOutput;
+			}
+			
 			$oDB = &DB::getInstance();
 			$oDB->begin();
+			
 			// messages to save in the sendor's message box
 			if($sender_srl && $sender_log) {
 				$output = executeQuery('communication.sendMessage', $sender_args);
@@ -147,6 +166,12 @@
 			if(!$output->toBool()) {
 				$oDB->rollback();
 				return $output;
+			}
+			// Call a trigger (after)
+			$trigger_output = ModuleHandler::triggerCall('communication.sendMessage', 'after', $trigger_obj);
+			if(!$trigger_output->toBool()){
+				$oDB->rollback();
+				return $trigger_output;
 			}
 			// create a flag that message is sent (in file format) 
 			$flag_path = './files/member_extra_info/new_message_flags/'.getNumberingPath($receiver_srl);
