530bf018459989d5e2509a348aaf5182044fff72
Beom Jinhyeok <dennis4079@gmail.com>
2015-02-09 01:55:38 +0900

    content 위젯의 RSS 리더 기능을 이용한 외부 리소스 실행 가능 취약점

--- config/func.inc.php
+++ config/func.inc.php
@@ -825,6 +825,20 @@
 
 		// xmp tag 확인 및 추가
 		$content = checkXmpTag($content);
+		$content = blockWidgetCode($content);
+		
+		return $content;
+	}
+	
+	/**
+	 * blocking widget code
+	 *
+	 * @param string $content Taget content
+	 * @return string
+	 **/
+	function blockWidgetCode($content){
+		$content = preg_replace('/(<(?:img|div)(?:[^>]*))(widget)(?:(=([^>]*?)>))/is', '$1blocked-widget$3', $content);
+		
 		return $content;
 	}
 
