d3489bc62419d23409f1047f2ee6258b0868d8e5
Beom Jinhyeok <dennis4079@gmail.com>
2015-05-03 23:51:19 +0900

    누락 코드 반영

--- modules/member/member.controller.php
+++ modules/member/member.controller.php
@@ -1032,6 +1032,8 @@
 			$auth_args->member_srl = $member_srl;
 			$auth_args->auth_key = $oPassword->createSecureSalt(40);
 
+			$oDB = &DB::getInstance();
+			$oDB->begin();
 			$output = executeQuery('member.updateAuthMail', $auth_args);
 			if(!$output->toBool()){
 				$oDB->rollback();
@@ -2159,9 +2161,11 @@
 			$auth_args = new stdClass;
 			$auth_args->user_id = $newEmail;
 			$auth_args->member_srl = $member_info->member_srl;
-			$auth_args->auth_key = md5(rand(0, 999999));
+			$auth_args->auth_key = $oPassword->createSecureSalt(40);
 			$auth_args->new_password = 'XE_change_emaill_address';
 
+			$oDB = &DB::getInstance();
+			$oDB->begin();
 			$output = executeQuery('member.insertAuthMail', $auth_args);
 			if(!$output->toBool()){
 				$oDB->rollback();
