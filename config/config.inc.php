6f84675b992f991ba6d07da91cd90469a7f1441e
YJSoft <yjsoft@yjsoft.pe.kr>
2015-09-04 17:01:46 +0900

    version up to 1.0.1.1

--- config/config.inc.php
+++ config/config.inc.php
@@ -20,7 +20,7 @@
 	/**
 	 * Display the version info of DAOL CMS.
 	 */
-	define('__DAOL_VERSION__', '1.0.1.0');
+	define('__DAOL_VERSION__', '1.0.1.1');
 	/**
 	 * Display the version info of XE(XDT EX).
 	 */
