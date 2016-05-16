fe445d03dfe83e866f0027c251ba3527fef89a1d
Beom Jinhyeok <dennis4079@gmail.com>
2016-05-17 00:29:13 +0900

    Edge 브라우저에서 파일 이름이 한글을 때 잘못된 파일명 문제 수정
    @Micalgenus

--- modules/file/file.controller.php
+++ modules/file/file.controller.php
@@ -292,7 +292,7 @@
 
 			$file_size = $file_obj->file_size;
 			$filename = $file_obj->source_filename;
-			if(strpos($_SERVER['HTTP_USER_AGENT'], 'MSIE') !== FALSE || (strpos($_SERVER['HTTP_USER_AGENT'], 'Windows') !== FALSE && strpos($_SERVER['HTTP_USER_AGENT'], 'Trident') !== FALSE && strpos($_SERVER['HTTP_USER_AGENT'], 'rv:') !== FALSE)){
+			if(strpos($_SERVER['HTTP_USER_AGENT'], 'MSIE') !== FALSE || strpos($_SERVER['HTTP_USER_AGENT'], 'Edge') !== FALSE || (strpos($_SERVER['HTTP_USER_AGENT'], 'Windows') !== FALSE && strpos($_SERVER['HTTP_USER_AGENT'], 'Trident') !== FALSE && strpos($_SERVER['HTTP_USER_AGENT'], 'rv:') !== FALSE)){
 				$filename = rawurlencode($filename);
 				$filename = preg_replace('/\./', '%2e', $filename, substr_count($filename, '.') - 1);
 			}
