f8679357303d8b875809a51d4a575a4f568e2f08
Beom Jinhyeok <dennis4079@gmail.com>
2016-08-23 00:46:01 +0900

    휴지통 복원 오류 수정

--- modules/comment/comment.controller.php
+++ modules/comment/comment.controller.php
@@ -218,7 +218,7 @@
 			if(!$logged_info->member_srl && !$obj->nick_name) return new Object(-1,'msg_invalid_request');
 
 			if(!$obj->comment_srl) $obj->comment_srl = getNextSequence();
-			elseif(!checkUserSequence($obj->comment_srl)) return new Object(-1, 'msg_not_permitted');
+			elseif(!$is_admin && !$manual_inserted && !checkUserSequence($obj->comment_srl)) return new Object(-1, 'msg_not_permitted');
 			
 			// determine the order
 			$obj->list_order = getNextSequence() * -1;
