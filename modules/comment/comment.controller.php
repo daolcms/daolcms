ea0206f848737f30ba3337467c1e1185cd882a14
Beom Jinhyeok <dennis4079@gmail.com>
2016-10-07 23:07:57 +0900

    익명 설정을 사용하는 경우, 글쓴이가 자신의 글을 추천, 비추천, 신고를 할 수 있는 문제 수정
    @sejin7940

--- modules/comment/comment.controller.php
+++ modules/comment/comment.controller.php
@@ -821,7 +821,7 @@
 				$oMemberModel = &getModel('member');
 				$member_srl = $oMemberModel->getLoggedMemberSrl();
 				// session registered if the author information matches to the current logged-in user's.
-				if($member_srl && $member_srl == $oComment->get('member_srl')) {
+				if($member_srl && $member_srl == abs($oComment->get('member_srl'))) {
 					$_SESSION['voted_comment'][$comment_srl] = true;
 					return new Object(-1, $failed_voted);
 				}
@@ -918,7 +918,7 @@
 				$oMemberModel = &getModel('member');
 				$member_srl = $oMemberModel->getLoggedMemberSrl();
 				// session registered if the author information matches to the current logged-in user's.
-				if($member_srl && $member_srl == $oComment->get('member_srl')) {
+				if($member_srl && $member_srl == abs($oComment->get('member_srl'))) {
 					$_SESSION['declared_comment'][$comment_srl] = true;
 					return new Object(-1, 'failed_declared');
 				}
