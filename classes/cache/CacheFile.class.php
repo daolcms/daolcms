e8cf869c59e7f95270e656768b9a80fddabf0729
Beom Jinhyeok <dennis4079@gmail.com>
2016-10-07 22:11:36 +0900

    변수명 오타 수정
    @qkrcjfgus33

--- classes/cache/CacheFile.class.php
+++ classes/cache/CacheFile.class.php
@@ -87,7 +87,7 @@ class CacheFile extends CacheBase{
 		$cache_file = $this->getCacheFileName($key);
 
 		if(file_exists($cache_file)){
-			if($modified_time > 0 && filemtime($cache_file) < $modified_timed){
+			if($modified_time > 0 && filemtime($cache_file) < $modified_time){
 				FileHandler::removeFile($cache_file);
 				return false;
 			}
@@ -110,7 +110,7 @@ class CacheFile extends CacheBase{
 			return false;
 		}
 
-		if($modified_time > 0 && filemtime($cache_file) < $modified_timed){
+		if($modified_time > 0 && filemtime($cache_file) < $modified_time){
 			FileHandler::removeFile($cache_file);
 			return false;
 		}
