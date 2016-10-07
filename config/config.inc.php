4fc3f10efe7fe723384196d6a6aabed4550a79f0
Beom Jinhyeok <dennis4079@gmail.com>
2016-10-07 23:15:16 +0900

    Version up to 1.0.2.2-test

--- config/config.inc.php
+++ config/config.inc.php
@@ -20,7 +20,7 @@
 	/**
 	 * Display the version info of DAOL CMS.
 	 */
-	define('__DAOL_VERSION__', '1.0.2.1');
+	define('__DAOL_VERSION__', '1.0.2.2-test');
 	/**
 	 * Display the version info of XE(XDT EX).
 	 */
