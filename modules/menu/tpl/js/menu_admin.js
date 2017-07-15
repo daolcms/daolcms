565016825b5db93514ec89e511c5eda6a8514081
Beom Jinhyeok <dennis4079@gmail.com>
2017-07-15 10:23:34 +0900

    #112 JSHint 경고 수정

--- modules/menu/tpl/js/menu_admin.js
+++ modules/menu/tpl/js/menu_admin.js
@@ -10,15 +10,15 @@ function doDeleteMenu(menu_srl) {
 
 /* 메뉴 삭제 후 */ 
 function completeDeleteMenu(ret_obj) {
-	var menu_srl = ret_obj['menu_srl'];
-	alert(ret_obj['message']);
+	var menu_srl = ret_obj.menu_srl;
+	alert(ret_obj.message);
 	location.href = current_url.setQuery('act','dispMenuAdminContent').setQuery('menu_srl','');
 } 
 
 /* 빈 메뉴 아이템 추가 */
 function doInsertMenuItem(parent_srl) {
 	if(typeof(parent_srl)=='undefined') parent_srl = 0;
-	var params = {node_srl:0, parent_srl:parent_srl}
+	var params = {node_srl:0, parent_srl:parent_srl};
 	doGetMenuItemInfo('menu', params);
 	deSelectNode();
 }
@@ -61,7 +61,7 @@ function completeGetMenuItemTplInfo(ret_obj, response_tags) {
 		obj.css('margin-top', 0);
 	}
 
-	var tpl = ret_obj['tpl'];
+	var tpl = ret_obj.tpl;
 	obj.html(tpl).show();
 }
 
@@ -75,15 +75,15 @@ function doMoveTree(menu_id, source_item, target_item) {
 	// 이동 취소를 선택하였을 경우 다시 그림;;
 	if(!procFilter(fo_obj, move_menu_item)) {
 		var params = [];
-		params["xml_file"] = jQuery('#fo_menu')[0].xml_file.value;
-		params["source_item"] = source_item;
+		params.xml_file = jQuery('#fo_menu')[0].xml_file.value;
+		params.source_item = source_item;
 		completeMoveMenuItem(params);
 	}
 }
 
 function completeMoveMenuItem(ret_obj) {
-	var source_item_srl = ret_obj['source_item_srl'];
-	var xml_file = ret_obj['xml_file'];
+	var source_item_srl = ret_obj.source_item_srl;
+	var xml_file = ret_obj.xml_file;
 
 	var fo_menu = jQuery("#fo_menu")[0];
 	if(!fo_menu) return;
@@ -94,8 +94,8 @@ function completeMoveMenuItem(ret_obj) {
 
 /* 메뉴 목록 갱신 */
 function doReloadTreeMenu(menu_srl) {
-	var params = new Array();
-	params["menu_srl"] = menu_srl;
+	var params = [];
+	params.menu_srl = menu_srl;
 
 	// 서버에 요청하여 해당 노드의 정보를 수정할 수 있도록 한다. 
 	var response_tags = new Array('error','message', 'xml_file', 'menu_title');
@@ -119,11 +119,11 @@ function doDeleteMenuItem(menu_item_srl) {
 
 /* 메뉴 아이템 삭제 후 */ 
 function completeDeleteMenuItem(ret_obj) {
-	var menu_title = ret_obj['menu_title'];
-	var menu_srl = ret_obj['menu_srl'];
-	var menu_item_srl = ret_obj['menu_item_srl'];
-	var xml_file = ret_obj['xml_file'];
-	alert(ret_obj['message']);
+	var menu_title = ret_obj.menu_title;
+	var menu_srl = ret_obj.menu_srl;
+	var menu_item_srl = ret_obj.menu_item_srl;
+	var xml_file = ret_obj.xml_file;
+	alert(ret_obj.message);
 
 	loadTreeMenu(xml_file, 'menu', 'menu_zone_menu', menu_title, '', doGetMenuItemInfo, menu_item_srl, doMoveTree);
 	jQuery('#menu_zone_info').html('');
@@ -181,11 +181,11 @@ function doDeleteButton(target) {
 
 	var col_name = target.replace(/^menu_/,'');
 
-	var params = new Array();
-	params['target'] = target;
-	params['menu_srl'] = fo_obj.menu_srl.value;
-	params['menu_item_srl'] = fo_obj.menu_item_srl.value;
-	params['filename'] = fo_obj[col_name].value;
+	var params = [];
+	params.target = target;
+	params.menu_srl = fo_obj.menu_srl.value;
+	params.menu_item_srl = fo_obj.menu_item_srl.value;
+	params.filename = fo_obj[col_name].value;
 
 	var response_tags = new Array('error','message', 'target');
 
@@ -193,7 +193,7 @@ function doDeleteButton(target) {
 }
 
 function completeDeleteButton(ret_obj, response_tags) {
-	var target = ret_obj['target'];
+	var target = ret_obj.target;
 	var column_name = target.replace(/^menu_/,'');
 
 	jQuery('#fo_menu')[0][column_name].value = '';
@@ -202,7 +202,7 @@ function completeDeleteButton(ret_obj, response_tags) {
 }
 /* 메뉴 입력후 */ 
 function completeInsertMenu(ret_obj) {
-	var menu_srl = ret_obj['menu_srl'];
-	alert(ret_obj['message']);
+	var menu_srl = ret_obj.menu_srl;
+	alert(ret_obj.message);
 	location.href = current_url.setQuery('act','dispMenuAdminContent');
 } 
