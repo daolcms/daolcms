f1145cc60cfe13c996c3eb1524bcc39dea2481ed
dorami <dennis4079@gmail.com>
2017-10-21 22:40:24 +0900

    구성이 변경된 JS 파일로 인한 스크립트 오류 문제 수정

--- common/js/common.js
+++ common/js/common.js
@@ -163,6 +163,8 @@ if(typeof window.XE == "undefined") {
 			var $this = $(this);
 			var href = $this.attr('href');
 			var target = $this.attr('target');
+			
+			if(!target || !href) return;
 
 			if(target === '_top' || target === '_self' || target === '_parent') {
 				$this.data('noopener', false);
@@ -184,6 +186,8 @@ if(typeof window.XE == "undefined") {
 		$('body').on('click', 'a[target]', function(e) {
 			var $this = $(this);
 			var href = $this.attr('href');
+			
+			if(!href) return;
 	
 			if($this.data('noopener') !== false && !window.XE.isSameHost(href)) {
 				var rel = $this.attr('rel');
