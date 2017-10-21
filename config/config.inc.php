7baeeb0970ba1a482f62cc49b99749797c45327b
dorami <dennis4079@gmail.com>
2017-10-22 00:45:53 +0900

    Version up to 1.0.3.6-test

--- config/config.inc.php
+++ config/config.inc.php
@@ -19,7 +19,7 @@ if(!defined('__XE__')) exit();
 /**
  * Display the version info of DAOL CMS.
  */
-define('__DAOL_VERSION__', '1.0.3.5');
+define('__DAOL_VERSION__', '1.0.3.6-test');
 
 /**
  * Display the version info of Based XE.
