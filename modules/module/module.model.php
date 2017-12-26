d4d2ef334df2fb91bbd5aea0daca8e3c495e3128
Beom Jinhyeok <dennis4079@gmail.com>
2017-12-27 00:09:11 +0900

    check_csrf() 사용 안함 옵션 추가

--- modules/module/module.model.php
+++ modules/module/module.model.php
@@ -615,6 +615,7 @@ class moduleModel extends module {
 					$standalone = $action->attrs->standalone == 'true' ? 'true' : 'false';
 					$ruleset = $action->attrs->ruleset ? $action->attrs->ruleset : '';
 					$method = $action->attrs->method ? $action->attrs->method : '';
+					$check_csrf = $action->attrs->check_csrf == 'false' ? 'false' : 'true';
 					
 					$index = $action->attrs->index;
 					$admin_index = $action->attrs->admin_index;
@@ -628,6 +629,7 @@ class moduleModel extends module {
 					$info->action->{$name}->standalone = $standalone == 'true' ? true : false;
 					$info->action->{$name}->ruleset = $ruleset;
 					$info->action->{$name}->method = $method;
+					$info->action->{$name}->check_csrf = $check_csrf;
 					if($action->attrs->menu_name) {
 						if($menu_index == 'true') {
 							$info->menu->{$action->attrs->menu_name}->index = $name;
@@ -648,6 +650,7 @@ class moduleModel extends module {
 					$buff[] = sprintf('$info->action->%s->standalone=\'%s\';', $name, $standalone);
 					$buff[] = sprintf('$info->action->%s->ruleset=\'%s\';', $name, $ruleset);
 					$buff[] = sprintf('$info->action->%s->method=\'%s\';', $name, $method);
+					$buff[] = sprintf('$info->action->%s->check_csrf=\'%s\';', $name, $check_csrf);
 					
 					if($index == 'true') {
 						$default_index_act = $name;
