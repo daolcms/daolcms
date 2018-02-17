b32d2f45e297f855262242f16e1c4bdfad553f22
Beom Jinhyeok <dennis4079@gmail.com>
2018-02-17 11:13:48 +0900

    관리자페이지 문서 검색대상에서 사용자정의 검색기능 추가

--- modules/document/document.model.php
+++ modules/document/document.model.php
@@ -228,7 +228,7 @@ class documentModel extends document {
 		} else {
 			// document.getDocumentList query execution
 			// Query_id if you have a group by clause getDocumentListWithinTag getDocumentListWithinComment or used again to perform the query because
-			$groupByQuery = array('document.getDocumentListWithinComment' => 1, 'document.getDocumentListWithinTag' => 1);
+			$groupByQuery = array('document.getDocumentListWithinComment' => 1, 'document.getDocumentListWithinTag' => 1, 'document.getDocumentListWithinExtraVars' => 1);
 			if(isset($groupByQuery[$query_id])) {
 				$group_args = clone($args);
 				$group_args->sort_index = 'documents.' . $args->sort_index;
@@ -1313,6 +1313,10 @@ class documentModel extends document {
 				case 'tag' :
 					$args->s_tags = str_replace(' ', '%', $search_keyword);
 					$query_id = 'document.getDocumentListWithinTag';
+					break;
+				case 'extra_vars':
+					$args->var_value = str_replace(' ', '%', $search_keyword);
+					$query_id = 'document.getDocumentListWithinExtraVars';
 					break;
 				default :
 					if(strpos($search_target, 'extra_vars') !== false) {
