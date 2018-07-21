06b639f3308a703e4ffbcc3b4a83b92ffbee38b5
Beom Jinhyeok <dennis4079@gmail.com>
2018-07-21 17:30:42 +0900

    Version up to 1.1.0.0-test5

--- config/config.inc.php
+++ config/config.inc.php
@@ -20,7 +20,7 @@ if(!defined('__XE__')) exit();
 /**
  * Display the version info of DAOL CMS.
  */
-define('__DAOL_VERSION__', '1.1.0.0-test4');
+define('__DAOL_VERSION__', '1.1.0.0-test5');
 
 /**
  * Display the version info of Based XE.
