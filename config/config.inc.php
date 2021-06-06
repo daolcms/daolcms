7e7178ca247644b06aa2c05a1029b02740117eac
Jinhyeok Beom <dennis4079@gmail.com>
2021-06-07 01:07:01 +0900

    #52 config.inc.php 오류 수정

--- config/config.inc.php
+++ config/config.inc.php
@@ -33,6 +33,10 @@ define('__XDT_EX_VERSION__', '1.5.4.7');
  * @deprecated this value will never changed, use __DAOL_VERSION__ instead.
  */
 define('__XE_VERSION__', '1.5.4.3');
+define('__XE_VERSION_ALPHA__', (stripos(__XE_VERSION__, 'alpha') !== false));
+define('__XE_VERSION_BETA__', (stripos(__XE_VERSION__, 'beta') !== false));
+define('__XE_VERSION_RC__', (stripos(__XE_VERSION__, 'rc') !== false));
+define('__XE_VERSION_STABLE__', (!__XE_VERSION_ALPHA__ && !__XE_VERSION_BETA__ && !__XE_VERSION_RC__));
 
 /**
  * Minimum version of PHP usable by DAOL CMS.
