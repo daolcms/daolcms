b2399c36c02b966a6eac11c21792e44125b8ce51
Jinhyeok Beom <dennis4079@gmail.com>
2022-10-10 22:05:19 +0900

    Version up to 1.1.0.8

--- config/config.inc.php
+++ config/config.inc.php
@@ -20,7 +20,7 @@ if(!defined('__XE__')) exit();
 /**
  * Display the version info of DAOL CMS.
  */
-define('__DAOL_VERSION__', '1.1.0.7');
+define('__DAOL_VERSION__', '1.1.0.8');
 
 /**
  * Display the version info of Based XDT EX.
