6043192c7a433eda0ff55c4e7edfd0368431ab3b
YJSoft <yjsoft@yjsoft.xyz>
2026-08-13 11:03:43 +0900

    RVE-2023-5 쪽지 답장 원문 권한 검증

--- modules/communication/communication.view.php
+++ modules/communication/communication.view.php
@@ -152,7 +152,7 @@ class communicationView extends communication {
 		$message_srl = Context::get('message_srl');
 		if($message_srl) {
 			$source_message = $oCommunicationModel->getSelectedMessage($message_srl);
-			if($source_message->message_srl == $message_srl && $source_message->sender_srl == $receiver_srl) {
+			if($source_message->message_srl == $message_srl && $source_message->sender_srl == $receiver_srl && $source_message->receiver_srl == $logged_info->member_srl) {
 				$source_message->title = "[re] " . $source_message->title;
 				$source_message->content = "\r\n<br />\r\n<br /><div style=\"padding-left:5px; border-left:5px solid #DDDDDD;\">" . trim($source_message->content) . "</div>";
 				Context::set('source_message', $source_message);
